72ce2ddfaf353aa0aea448493bf233e807c60d5c
PrajwalSahu <116376785+PRgb2022@users.noreply.github.com>
2025-07-08 14:33:12 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,7 +1,7 @@
 db_config = {
-    'host': '127.0.0.1',
-    'port': 3306,
+    'host': '127.0.0.1',       
+    'port': 3306,              
     'user': 'root',
     'password': 'Prajwal.sql@25',
-    'database': 'alerts.db'
+    'database': 'ndma_alerts'
 }
